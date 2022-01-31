9b139c20cb8dd65ca675617a50ae75d79c18368e
LevonHasratyan <levon.hasratyan5@mail.ru>
2022-01-31 16:53:54 +0400

    hello

---
+++ housesale/assets/js/script.js
@@ -0,0 +1,31 @@
+function myFunction(x) {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  //  let swiper = new Swiper('.swiper', {
+  //   // Optional parameters
+    
+  //   loop: true,
+  
+  //   // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+  
+  //   // Navigation arrows
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  
+  //   // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //     scrollbar: false,
+  //   },
+  // });
+    
